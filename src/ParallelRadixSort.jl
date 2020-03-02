c783046eefe57eaff3d4a92637b470ce8271ac6d
Takafumi Arakaki <aka.tkf@gmail.com>
2020-03-02 13:22:58 -0800

    Files generated by PkgTemplates

---
+++ src/ParallelRadixSort.jl
@@ -0,0 +1,5 @@
+module ParallelRadixSort
+
+greet() = print("Hello World!")
+
+end # module
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using ParallelRadixSort
+using Test
+
+@testset "ParallelRadixSort.jl" begin
+    # Write your own tests here.
+end
