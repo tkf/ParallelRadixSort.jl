d65fe66c7a2d825360a686740e4cae5c4659e418
Takafumi Arakaki <aka.tkf@gmail.com>
2021-10-30 16:46:30 -0400

    Use Tapir if available

--- src/ParallelRadixSort.jl
+++ src/ParallelRadixSort.jl
@@ -7,11 +7,16 @@ export MSDRadixSort, ParallelMSDRadixSort, ParallelStableMSDRadixSort, StableMSD
 # end
 
 using ArgCheck: @check
-using Base.Threads: @spawn
 using Base.Order: Order, Ordering, ForwardOrdering, ReverseOrdering
 using ConstructionBase: setproperties
 using StaticArrays: MVector
 using StaticNumbers: @stat, StaticInteger, maybe_static, static
+
+if isdefined(Base.Experimental, :Tapir)
+    using Base.Experimental.Tapir: @sync, @spawn
+else
+    using Base.Threads: @sync, @spawn
+end
 
 abstract type RadixSortAlgorithm <: Base.Sort.Algorithm end
 
