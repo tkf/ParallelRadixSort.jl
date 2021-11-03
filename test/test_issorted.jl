1f89fdf0e57baad8b3b76e3618a6952ca3c86393
Takafumi Arakaki <aka.tkf@gmail.com>
2021-11-03 01:18:02 -0700

    Test with large inputs (#7)

--- test/test_issorted.jl
+++ test/test_issorted.jl
@@ -4,12 +4,11 @@ using ParallelRadixSort
 using Test
 
 testdata_integers = [
-    ("$T[...]", rand(T(0):T(9), 100)) for
+    ("$T[...]", () -> rand(T, 2^20)) for
     T in [UInt8, UInt16, UInt32, UInt64, Int8, Int16, Int32, Int64]
 ]
 
-testdata_floats =
-    [("$T[...]", rand(range(T(0), T(1), length = 11), 100)) for T in [Float32, Float64]]
+testdata_floats = [("$T[...]", () -> rand(T, 2^20)) for T in [Float32, Float64]]
 
 raw_testdata = """
 rand('a':'z', 100)
@@ -18,22 +17,27 @@ rand('a':'z', 100)
 [rand(1:10, 10) for _ in 1:100]
 """
 
-# A vector of `(label, vector)`
+# A vector of `(datalabel, function)`
 testdata_misc = map(split(raw_testdata, "\n", keepempty = false)) do code
-    @debug "Evaluating: $code"
-    (code, Base.include_string(@__MODULE__, code))
+    fn = "() -> $code"
+    @debug "Evaluating: $fn"
+    (code, Base.include_string(@__MODULE__, fn))
 end
 
 testdata = vcat(testdata_integers, testdata_floats, testdata_misc)
 
-@testset "$algname" for algname in [
-    :MSDRadixSort,
-    :StableMSDRadixSort,
-    :ParallelMSDRadixSort,
-    :ParallelStableMSDRadixSort,
+@testset "$alglabel" for (alglabel, alg) in [
+    (:MSDRadixSort, ParallelRadixSort.MSDRadixSort),
+    (:StableMSDRadixSort, ParallelRadixSort.StableMSDRadixSort),
+    (:ParallelMSDRadixSort, ParallelRadixSort.ParallelMSDRadixSort),
+    (:ParallelStableMSDRadixSort, ParallelRadixSort.ParallelStableMSDRadixSort),
+    (
+        "ParallelMSDRadixSort(basesize = 2^10)",
+        ParallelRadixSort.ParallelMSDRadixSort(basesize = 2^10),
+    ),
 ]
-    @testset "$label" for (label, vector) in testdata
-        alg = getfield(ParallelRadixSort, algname)
+    @testset "$datalabel" for (datalabel, fn) in testdata
+        vector = fn()
         @test issorted(sort(vector; alg = alg))
         eltype(vector) isa Number || continue
         @test issorted(sort(vector; alg = alg, rev = true); rev = true)
