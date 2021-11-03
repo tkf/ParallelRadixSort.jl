afe501ecf7f31b3538ce9455e46e5ce76b613fd0
Takafumi Arakaki <aka.tkf@gmail.com>
2021-11-03 03:46:39 -0400

    Test with large inputs

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,19 @@
 module TestParallelRadixSort
 using Test
 
+if lowercase(get(ENV, "CI", "false")) == "true" && VERSION ≥ v"1.7-"
+    # Use https://github.com/perrutquist/StaticNumbers.jl/pull/12 as otherwise
+    # the test would be extremely slow.
+    Pkg = Base.require(Base.PkgId(Base.UUID(0x44cfe95a1eb252eab672e2afdf69b78f), "Pkg"))
+    Pkg.add(
+        Pkg.PackageSpec(;
+            name = "StaticNumbers",
+            url = "https://github.com/tkf/StaticNumbers.jl.git",
+            rev = "kwargs.data",
+        ),
+    )
+end
+
 @testset "$file" for file in sort([
     file for file in readdir(@__DIR__) if match(r"^test_.*\.jl$", file) !== nothing
 ])
--- test/test_issorted.jl
+++ test/test_issorted.jl
@@ -4,12 +4,11 @@ using ParallelRadixSort
 using Test
 
 testdata_integers = [
-    ("$T[...]", rand(T(0):T(9), 100)) for
+    ("$T[...]", () -> rand(T, 2^20)) for
     T in [UInt8, UInt16, UInt32, UInt64, Int8, Int16, Int32, Int64]
 ]
 
-testdata_floats =
-    [("$T[...]", rand(range(T(0), T(1), length = 11), 100)) for T in [Float32, Float64]]
+testdata_floats = [("$T[...]", () -> rand(T, 2^20)) for T in [Float32, Float64]]
 
 raw_testdata = """
 rand('a':'z', 100)
@@ -18,10 +17,11 @@ rand('a':'z', 100)
 [rand(1:10, 10) for _ in 1:100]
 """
 
-# A vector of `(label, vector)`
+# A vector of `(label, function)`
 testdata_misc = map(split(raw_testdata, "\n", keepempty = false)) do code
-    @debug "Evaluating: $code"
-    (code, Base.include_string(@__MODULE__, code))
+    fn = "() -> $code"
+    @debug "Evaluating: $fn"
+    (code, Base.include_string(@__MODULE__, fn))
 end
 
 testdata = vcat(testdata_integers, testdata_floats, testdata_misc)
@@ -32,7 +32,8 @@ testdata = vcat(testdata_integers, testdata_floats, testdata_misc)
     :ParallelMSDRadixSort,
     :ParallelStableMSDRadixSort,
 ]
-    @testset "$label" for (label, vector) in testdata
+    @testset "$label" for (label, fn) in testdata
+        vector = fn()
         alg = getfield(ParallelRadixSort, algname)
         @test issorted(sort(vector; alg = alg))
         eltype(vector) isa Number || continue
